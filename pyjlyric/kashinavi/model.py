c623f9903177eabc6bd8fa53bdff8fcec4c9245f
eggplants <w10776e8w@yahoo.co.jp>
2023-07-28 01:53:25 +0900

    chore: pydantic v2

--- pyjlyric/kashinavi/model.py
+++ pyjlyric/kashinavi/model.py
@@ -4,33 +4,33 @@ from pyjlyric.model import LyricPage, WithUrlText
 
 
 class _ByArtist(BaseModel):
-    _type: str = Field(..., alias="@type")
-    name: str
+    type_: str = Field(..., alias="@type")
+    name_: str
     url: HttpUrl
 
 
 class _RecordedAs(BaseModel):
-    _type: str = Field(..., alias="@type")
+    type_: str = Field(..., alias="@type")
     by_artist: _ByArtist = Field(..., alias="byArtist")
 
 
 class _Lyricist(BaseModel):
-    _type: str = Field(..., alias="@type")
+    type_: str = Field(..., alias="@type")
     name: str
 
 
 class _Composer(BaseModel):
-    _type: str = Field(..., alias="@type")
+    type_: str = Field(..., alias="@type")
     name: str
 
 
 class _MainEntityOfPage(BaseModel):
-    _type: str = Field(..., alias="@type")
+    type_: str = Field(..., alias="@type")
     url: HttpUrl
 
 
 class _Lyrics(BaseModel):
-    _type: str = Field(..., alias="@type")
+    type_: str = Field(..., alias="@type")
     main_entity_of_page: _MainEntityOfPage = Field(..., alias="mainEntityOfPage")
     text: str
 
