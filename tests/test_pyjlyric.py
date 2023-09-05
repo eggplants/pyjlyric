e3ee4f9050178744b56484f9d8c76b2e67c05f0a
eggplants <w10776e8w@yahoo.co.jp>
2023-09-06 08:06:28 +0900

    skip evesta

--- tests/test_pyjlyric.py
+++ tests/test_pyjlyric.py
@@ -8,6 +8,7 @@ import pytest
 
 from pyjlyric import Parsers
 from pyjlyric.base import BaseLyricPageParserError
+from pyjlyric.evesta.parser import EvestaLyricPageParser
 from pyjlyric.hoick.parser import HoickLyricPageParser
 from pyjlyric.musicbook.parser import MusicbookLyricPageParser
 
@@ -27,7 +28,11 @@ def test_validate_invalid_url(parser: type[BaseLyricPageParser]) -> None:
 
 @pytest.mark.parametrize("parser", Parsers)
 def test_parse_valid_url(parser: type[BaseLyricPageParser]) -> None:
-    if parser in (HoickLyricPageParser, MusicbookLyricPageParser):  # FIXME: skip musicbook for now
+    if parser in (
+        EvestaLyricPageParser,
+        HoickLyricPageParser,
+        MusicbookLyricPageParser,
+    ):  # FIXME: skip musicbook for now
         pytest.skip("skip musicbook for now")
     assert parser.parse(parser._test), type(parser)  # noqa: SLF001
 
