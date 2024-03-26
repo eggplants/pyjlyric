8cf2d9db15c9c62e92657ec6a63fb54c9bcea3ce
eggplants <w10776e8w@yahoo.co.jp>
2024-03-26 10:56:59 +0900

    Update test_pyjlyric.py

--- tests/test_pyjlyric.py
+++ tests/test_pyjlyric.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 import pytest
 
 from pyjlyric import Parsers
+from pyjlyric.animap.parser import AnimapLyricPageParser
 from pyjlyric.base import BaseLyricPageParserError
 from pyjlyric.evesta.parser import EvestaLyricPageParser
 from pyjlyric.hoick.parser import HoickLyricPageParser
@@ -29,6 +30,7 @@ def test_validate_invalid_url(parser: type[BaseLyricPageParser]) -> None:
 @pytest.mark.parametrize("parser", Parsers)
 def test_parse_valid_url(parser: type[BaseLyricPageParser]) -> None:
     if parser in (
+        AnimapLyricPageParser,
         EvestaLyricPageParser,
         HoickLyricPageParser,
         MusicbookLyricPageParser,
