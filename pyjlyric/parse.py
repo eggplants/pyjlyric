0d5e8350f0fd3be5ecd48f104e7ce26fb7e197bc
eggplants <w10776e8w@yahoo.co.jp>
2024-12-23 00:41:43 +0900

    fix output format

--- pyjlyric/parse.py
+++ pyjlyric/parse.py
@@ -61,23 +61,29 @@ def get_lyric_text(url: str) -> str | None:
 
     title = r.title
     artist = r.artist.text if isinstance(r.artist, WithUrlText) else r.artist
-    artist = [a.text if isinstance(a, WithUrlText) else a for a in artist] if isinstance(artist, list) else artist
+    artist = (
+        ", ".join([a.text if isinstance(a, WithUrlText) else a for a in artist]) if isinstance(artist, list) else artist
+    )
 
     lyricist = r.lyricist.text if isinstance(r.lyricist, WithUrlText) else r.lyricist
     lyricist = (
-        [ly.text if isinstance(ly, WithUrlText) else ly for ly in lyricist] if isinstance(lyricist, list) else lyricist
+        ", ".join([ly.text if isinstance(ly, WithUrlText) else ly for ly in lyricist])
+        if isinstance(lyricist, list)
+        else lyricist
     )
 
     composer = r.composer.text if isinstance(r.composer, WithUrlText) else r.composer
     composer = (
-        [c.text if isinstance(c, WithUrlText) else c for c in composer] if isinstance(composer, list) else composer
+        ", ".join([c.text if isinstance(c, WithUrlText) else c for c in composer])
+        if isinstance(composer, list)
+        else composer
     )
 
     lyric_sections = [linesep.join(paragraph) for paragraph in r.lyric_sections]
 
     return (
         textwrap.dedent(
-            f"""\
+            f"""
             ===
             Title:\t\t{title}
             Artist:\t\t{artist or "(No data)"}
@@ -87,4 +93,4 @@ def get_lyric_text(url: str) -> str | None:
             """,
         )
         + (linesep * 2).join(lyric_sections).strip()
-    )
+    ).strip()
